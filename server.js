1d877e457c38cf32928e4b92341fe6882f9d23ae
Dsaar <danieljoshuasaar@gmail.com>
2025-07-25 10:04:05 +0300

    add cors policy

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import logger from './middlewares/logger.js';
 import router from './router/router.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+
+	origin: ["http://127.0.0.1:5500"],
+
+}));
 
 app.use(express.json());
 app.use(logger);
