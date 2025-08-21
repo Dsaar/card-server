be1d7d17905b31a08ff4c6fc53ab7b6743535eac
Dsaar <danieljoshuasaar@gmail.com>
2025-08-21 15:06:37 +0300

    handle error in login path

--- users/services/usersService.js
+++ users/services/usersService.js
@@ -35,10 +35,14 @@ export const createNewUser = async (user) => {
 
 //log in
 export const login = async (email, password) => {
-	const user = await getUserByEmail(email)
-	if (comparePassword(password, user?.password)) {
-		return generateToken(user)
+	try {
+		const user = await getUserByEmail(email);
+		if (comparePassword(password, user?.password)) {
+			return generateToken(user);
+		}
+		throw new Error("password incorrect");
+	} catch (error) {
+		throw new Error(error.message);
 	}
-	return null;
 };
 
